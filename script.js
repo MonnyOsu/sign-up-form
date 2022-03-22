644ddaa36d9ad57dc38aa1dbb9cbe5449d910a1e
Eamonn O'Sullivan <eamonnsosullivan@gmail.com>
2022-03-22 19:47:42 +1100

    sign-up form with basic validation

---
+++ script.js
@@ -0,0 +1,24 @@
+password = document.querySelector('[data-password]');
+confirmPassword = document.querySelector('[data-confirm-password]');
+signUpForm = document.querySelector('[data-sign-up-form]')
+submitButton = document.querySelector('[data-submit-button]')
+
+
+function checkPassword() {
+    let pword = password.value;
+    let confirm = confirmPassword.value;
+
+    if (pword == confirm) {
+        confirmPassword.style.border = "2px solid green";
+        submitButton.disabled = false
+    } else {
+        confirmPassword.style.border = "2px solid red";
+        submitButton.disabled = true;
+    }
+
+}
+
+confirmPassword.addEventListener('input', (e) => {
+    checkPassword();
+})
+
